b0d5cbc79ab2d94649baaa011b68cb58753773cd
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-09-29 16:48:24 +0900

    UPDATE: install test.check package & specify main ns

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/radish-miyazaki/hangman-clojure"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/test.check "1.1.1"]]
+  :main hangman.core
   :repl-options {:init-ns hangman.core})
 
