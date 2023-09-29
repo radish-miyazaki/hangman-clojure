07df974889348597073162a8e39b3f6074083194
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-09-29 12:09:56 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject hangman "0.1.0-SNAPSHOT"
+  :description "Hagman game in Clojure"
+  :url "https://github.com/radish-miyazaki/hangman-clojure"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns hangman.core})
+
