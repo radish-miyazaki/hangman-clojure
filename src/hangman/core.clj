dabe249e729b89c7a79c9f420905ea0d4ed4904b
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-09-29 18:36:32 +0900

    FIX: refactoring (function orders)

--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -2,105 +2,55 @@
   (:require
    [clojure.java.io :as jio]
    [clojure.spec.alpha :as s]
-   [clojure.spec.gen.alpha :as gen]
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [clojure.test.check.generators :as gen]))
 
-(defonce letters (mapv char (range (int \a) (inc (int \z)))))
-
+;; --------------------
+;; protocols & records
+;; --------------------
 (defprotocol Player
   (next-guess [player progress]))
 
 (defrecord ChoicesPlayer [choices]
-             Player
-             (next-guess [_ _]
-               (let [guess (first @choices)]
-                 (swap! choices rest)
-                 guess)))
+  Player
+  (next-guess [_ _]
+    (let [guess (first @choices)]
+      (swap! choices rest)
+      guess)))
 
-(defn choices-player [choices]
-  (->ChoicesPlayer (atom choices)))
+;; --------------------
+;; specs
+;; --------------------
+(defonce letters (mapv char (range (int \a) (inc (int \z)))))
 
-(defn alpha-player []
-  (choices-player letters))
-
-(defn freq-player []
-  (choices-player (seq "etooinshrdlcumwfgypbvkjxqz")))
-
-(defn shuffled-player []
-  (choices-player (shuffle letters)))
-
-(defn random-letter []
+(defn- random-letter []
   (rand-nth letters))
 
 (def random-player
   (reify Player
     (next-guess [_ _] (random-letter))))
 
-(defn new-progress [word]
-  (repeat (count word) \_))
+(defn- choices-player [choices]
+  (->ChoicesPlayer (atom choices)))
 
-(defn update-progress [progress word guess]
-  (map #(if (= %1 guess) guess %2) word progress))
+(defn- alpha-player []
+  (choices-player letters))
 
-(defn complete? [progress word]
-  (= progress (seq word)))
+(defn- freq-player []
+  (choices-player (seq "etooinshrdlcumwfgypbvkjxqz")))
 
-(defn valid-letter? [c]
+(defn- shuffled-player []
+  (choices-player (shuffle letters)))
+
+(defn- valid-letter? [c]
   (<= (int \a) (int c) (int \z)))
 
-(defonce available-words
-  (with-open [r (jio/reader "words.txt")]
-    (->> (line-seq r)
-         (filter #(every? valid-letter? %))
-         vec)))
-
-(defn random-word []
-  (rand-nth available-words))
-
-(defn report [begin-progress guess end-progress]
-  (println)
-  (println "You guessed: " guess)
-  (if (= begin-progress end-progress)
-    (if (some #{guess} end-progress)
-      (println "Sorry, you already guessed: " guess)
-      (println "Sorry, the word does not contain: " guess))
-    (println "The letter " guess " is in the word!"))
-  (println "Progress so far: " (apply str end-progress)))
-
-(defn game
-  [word player & {:keys [verbose] :or {verbose false}}]
-  (when verbose
-    (println "You are guessing a word with" (count word) "letters."))
-  (loop [progress (new-progress word) guesses 1]
-    (let [guess (next-guess player progress)
-          progress' (update-progress progress word guess)]
-     (when verbose (report progress guess progress'))
-     (if (complete? progress' word)
-       guesses
-       (recur progress' (inc guesses))))))
-
-(defn take-guess []
-  (println)
-  (println "Enter a letter: ")
-  (flush)
-  (let [input (.readLine *in*)
-        line (str/trim input)]
-    (cond
-      (str/blank? line) (recur)
-      (valid-letter? (first line)) (first line)
-      :else (do
-              (println "That is not a valid letter!")
-              (recur)))))
-
-(def interactive-player
-  (reify Player
-    (next-guess [_ _] (take-guess))))
-
-;; --------------------
-;; specs
-;; --------------------
-(defn player? [p]
+(defn- player? [p]
   (satisfies? Player p))
+
+(defn- letters-left
+  [progress]
+  (->> progress (keep #{\_}) count))
 
 (s/def ::player
   (s/with-gen player?
@@ -124,10 +74,6 @@
 (s/def ::verbose (s/with-gen boolean? #(s/gen false?)))
 (s/def ::score pos-int?)
 
-(defn- letters-left
-  [progress]
-  (->> progress (keep #{\_}) count))
-
 (s/fdef hangman.core/new-progress
   :args (s/cat :word ::word)
   :ret ::progress
@@ -149,8 +95,68 @@
   :ret ::score)
 
 ;; --------------------
+;; core
+;; --------------------
+(defn new-progress [word]
+  (repeat (count word) \_))
+
+(defn update-progress [progress word guess]
+  (map #(if (= %1 guess) guess %2) word progress))
+
+(defn complete? [progress word]
+  (= progress (seq word)))
+
+(defn report [begin-progress guess end-progress]
+  (println)
+  (println "You guessed: " guess)
+  (if (= begin-progress end-progress)
+    (if (some #{guess} end-progress)
+      (println "Sorry, you already guessed: " guess)
+      (println "Sorry, the word does not contain: " guess))
+    (println "The letter " guess " is in the word!"))
+  (println "Progress so far: " (apply str end-progress)))
+
+(defn game
+  [word player & {:keys [verbose] :or {verbose false}}]
+  (when verbose
+    (println "You are guessing a word with" (count word) "letters."))
+  (loop [progress (new-progress word) guesses 1]
+    (let [guess (next-guess player progress)
+          progress' (update-progress progress word guess)]
+     (when verbose (report progress guess progress'))
+     (if (complete? progress' word)
+       guesses
+       (recur progress' (inc guesses))))))
+
+;; --------------------
 ;; main
 ;; --------------------
+(defonce available-words
+  (with-open [r (jio/reader "words.txt")]
+    (->> (line-seq r)
+         (filter #(every? valid-letter? %))
+         vec)))
+
+(defn random-word []
+  (rand-nth available-words))
+
+(defn take-guess []
+  (println)
+  (println "Enter a letter: ")
+  (flush)
+  (let [input (.readLine *in*)
+        line (str/trim input)]
+    (cond
+      (str/blank? line) (recur)
+      (valid-letter? (first line)) (first line)
+      :else (do
+              (println "That is not a valid letter!")
+              (recur)))))
+
+(def interactive-player
+  (reify Player
+    (next-guess [_ _] (take-guess))))
+
 (defn -main []
   (game (random-word) interactive-player :verbose true))
 
